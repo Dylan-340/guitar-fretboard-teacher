e27e584bbe5e94cb6f36f132e0345cd74dacc7ff
Dylan-340 <dylansb1126@gmail.com>
2026-03-04 16:59:16 -0500

    Add files via upload
    updated both functions, and added comments

--- fretboardlearning.py
+++ fretboardlearning.py
@@ -1,6 +1,6 @@
 # at this point the first 20 or so lines of code is a 3.5/47 according to gemma3. must get a higher rating at the end.
  
-#Program to help learn the guitar fretboard.
+# Program to help learn the guitar fretboard.
 
 from time import sleep
 
@@ -18,37 +18,51 @@ print("ctrl-c to exit\n")
 print("--Guitar Fretboard Teacher--\n")
 
 
-def manual_setup():                                     # Asks the user if they want the notes to scroll automatically or manually
-    print("Do you want auto or manual notes?\n")
+def manual_setup():        # Asks the user if they want the notes to scroll automatically or manually, and gets the automatic delay if chosen, returns the bool of if manual mode is active, and if not the auto delay for the strings.
+    print("Do you want auto or manual notes?\n")       
     manual_input = input("[a/m]: ").lower()
     while manual_input != "a" and manual_input != "m":
         print("error, answer must be an a or m")
         manual_input = input("[a/m]: ").lower()
     manual_input = (manual_input == 'm')
-    return manual_input
 
-def fret_hints_setup():                                 # Asks the user if they want the notes to show the coresponding fret number
-    
-    fret_hints_input = input("Do you want fret hints?\n[y/n]:").lower()
-    while fret_hints_input != "y" and fret_hints_input != "n":
-        print("error, answer must be a y or n")
-        fret_hints_input = input("[y/n]: ").lower()       
-    fret_hints_input = (fret_hints_input == 'y')
-    return fret_hints_input
-    
-    
-manual_input = manual_setup() 
-#fret_hints_input = fret_hints_setup()
+    if manual_input != True:
 
-
-if manual_input != True:
-
-    str_note_delay = input("What delay in seconds would you like?: ") # Sets the delay between notes being printed if automatic notes are chosen
-    if str_note_delay == "": # If user leaves delay blank it defaults to 1 second
+        str_note_delay = input("What delay in seconds would you like?: ") # Sets the delay between notes being printed if automatic notes are chosen
+    if str_note_delay == "":                                          # If user leaves delay blank it defaults to 1 second
         note_delay = 1
     else:
-        note_delay = float(str_note_delay) 
+        note_delay = float(str_note_delay)
 
+    return manual_input, note_delay
+
+def fret_hints_setup():                                 # Asks the user if they want the notes to show the coresponding fret number, returns the bool of if fret hints are active, and the string for the fret hints.
+
+    fret_hints_input = False
+    used_string = ""
+
+    fret_hints_input = input("Do you want fret hints?\n[y/n]:").lower()
+
+    while fret_hints_input != "y" and fret_hints_input != "n":
+        print("error, answer must be a y or n")
+        fret_hints_input = input("[y/n]: ").lower()      
+
+    fret_hints_input = (fret_hints_input == 'y')
+
+    if fret_hints_input == True:
+        used_string = input("What string would you like the hints for? [E/A/D/G/B]").upper()
+        while used_string != "E" and used_string != "A" and used_string != "D" and used_string != "G" and used_string != "B":
+            print("error, answer must be an e, a, d, g, or b")
+            used_string = input("What string would you like the hints for? [E/A/D/G/B]").upper()
+
+    return fret_hints_input, used_string
+    
+    
+manual_input, note_delay = manual_setup() 
+fret_hints_input, used_string = fret_hints_setup()
+
+#print(fret_hints_input, used_string)
+ 
 
 
 while True:
