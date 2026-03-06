c5648fdf58924a9c4f826fc7c1d5240b5b7ba784
Dylan-340 <dylansb1126@gmail.com>
2026-03-05 19:01:16 -0500

    Add files via upload

--- fretboardlearning.py
+++ fretboardlearning.py
@@ -13,6 +13,14 @@ dstring_frets = ["7", "9", "10", "12", "2", "3", "5", "8", "11", "1", "4", "6"] 
 gstring_frets = ["2", "4", "5", "7", "9", "10", "12", "3", "6", "8", "11", "1"] # Fret numbers for g string
 bstring_frets = ["10", "12", "1", "3", "5", "6", "8", "11", "2", "4", "7", "9"] # Fret numbers for b string
 
+string_frets = {
+    "e": estring_frets,
+    "a": astring_frets,
+    "d": dstring_frets,
+    "g": gstring_frets,
+    "b": bstring_frets
+}
+
 print("ctrl-c to exit\n")
 
 print("--Guitar Fretboard Teacher--\n")
@@ -33,7 +41,7 @@ def manual_setup():        # Asks the user if they want the notes to scroll auto
     if manual_input != True:
 
         str_note_delay = input("What delay in seconds would you like?: ") # Sets the delay between notes being printed if automatic notes are chosen
-    if str_note_delay == "":                                          # If user leaves delay blank it defaults to 1 second
+    if str_note_delay == "":                                              # If user leaves delay blank it defaults to 1 second
         note_delay = 1
     else:
         note_delay = float(str_note_delay)
@@ -54,26 +62,41 @@ def fret_hints_setup():                                 # Asks the user if they 
     fret_hints_input = (fret_hints_input == 'y')
 
     if fret_hints_input == True:
-        used_string = input("What string would you like the hints for? [E/A/D/G/B]").upper()
-        while used_string != "E" and used_string != "A" and used_string != "D" and used_string != "G" and used_string != "B":
+        used_string = input("What string would you like the hints for? [E/A/D/G/B]").lower()
+        while used_string != "e" and used_string != "a" and used_string != "d" and used_string != "g" and used_string != "b":
             print("error, answer must be an e, a, d, g, or b")
             used_string = input("What string would you like the hints for? [E/A/D/G/B]").upper()
 
     return fret_hints_input, used_string
     
-    
-manual_input, note_delay = manual_setup() 
-fret_hints_input, used_string = fret_hints_setup()
+def manual_notes():             # Runs if manual mode is active
+    while True:
+        if fret_hints_input == True:
+            input()
+            index = random.randint(0, 11)
+            print(notes[index], selected_fret_hints[index])
+        else:
+            input()
+            print(random.choice(notes), "\n")
 
-#print(fret_hints_input, used_string)
- 
+def automatic_notes():          # Runs if automatic mode is active
+    while True:
+        if fret_hints_input == True:
+            index = random.randint(0, 11)
+            print(notes[index], selected_fret_hints[index], "\n")
+            sleep(note_delay)
+        else:
+            print(random.choice(notes), "\n")
+            sleep(note_delay)
 
+manual_input, note_delay = manual_setup() # get the variables from the manual setup function
+fret_hints_input, used_string = fret_hints_setup() # gets the variables from the fret hints setup
 
-while True:
+if fret_hints_input == True:        # chooses the string that should be used for fret hints
+    selected_fret_hints = string_frets[used_string]
 
-    if manual_input == True:
-        input()
-        print(random.choice(notes))
-    else:
-        print(random.choice(notes), "\n")
-        sleep(note_delay)
+if manual_input == True:            # runs the proper auto/manual mode
+    manual_notes()
+else:
+    print("auto")
+    automatic_notes()
